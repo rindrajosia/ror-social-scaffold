045d9c3d8ea87830839621b5764daa468f15ec44
rindra josia <rrindrajosia@gmail.com>
2020-06-26 11:18:50 +0300

    fix rubocop error

--- Gemfile
+++ Gemfile
@@ -41,15 +41,15 @@ gem 'devise'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'database_cleaner'
   gem 'rspec-rails', '~> 4.0.0'
-  gem "database_cleaner"
 end
 
 group :test do
-  gem 'rspec'
+  gem 'capybara'
   gem 'database_cleaner-active_record'
-  gem "capybara"
-  gem "selenium-webdriver"
+  gem 'rspec'
+  gem 'selenium-webdriver'
 end
 
 group :development do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,49 +2,41 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-  @users = []
-  User.all.each do |user|
-    if !friend?(user)
-      @users << user
+    @users = []
+    User.all.each do |user|
+      @users << user unless friend?(user)
     end
-  end
-  @friends_confirmed = friends_confirmed
-  @friends_inverse_confirmed = friends_inverse_confirmed
-  @requests = friend_requests
-  @invitations = friend_requests_by_me
+    @friends_confirmed = friends_confirmed
+    @friends_inverse_confirmed = friends_inverse_confirmed
+    @requests = friend_requests
+    @invitations = friend_requests_by_me
 end
 
-def show
-  @user = User.find(params[:id])
-  @posts = @user.posts.ordered_by_most_recent
-  if !friend?(@user)
-    @friend = true
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.ordered_by_most_recent
+    @friend = true unless friend?(@user)
   end
-end
 
-def friend_requests
-  friendship_array = []
+  def friend_requests
+    friendship_array = []
     current_user.inverse_friendships.each do |friendship|
-      if friendship.status == false
-        friendship_array << friendship
-      end
+      friendship_array << friendship if friendship.status == false
     end
     friendship_array
-end
+  end
 
-def friend_requests_by_me
-  friendship_array = []
+  def friend_requests_by_me
+    friendship_array = []
     current_user.friendships.each do |friendship|
-      if friendship.status == false
-        friendship_array << friendship.friend
-      end
+      friendship_array << friendship.friend if friendship.status == false
     end
     friendship_array
-end
+  end
 
-def friends
-  friendship_array = []
-  friend = []
+  def friends
+    friendship_array = []
+    friend = []
     current_user.friendships.each do |friendship|
       friendship_array << friendship.friend
     end
@@ -52,30 +44,25 @@ def friends
       friend << user
     end
     friendship_array + friend
-end
+  end
 
-def friends_inverse_confirmed
-  friendship_array = []
+  def friends_inverse_confirmed
+    friendship_array = []
     current_user.friendships.each do |friendship|
-      if friendship.status == true
-        friendship_array << friendship
-      end
+      friendship_array << friendship if friendship.status == true
     end
     friendship_array
-end
+  end
 
-def friends_confirmed
-  friend = []
+  def friends_confirmed
+    friend = []
     current_user.inverse_friendships.each do |friendship|
-      if friendship.status == true
-        friend << friendship
-      end
+      friend << friendship if friendship.status == true
     end
     friend
-end
+  end
 
-def friend?(user)
-  friends.include?(user)
-end
-
+  def friend?(user)
+    friends.include?(user)
+  end
 end

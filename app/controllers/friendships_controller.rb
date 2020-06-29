f86991ec4e35aa75718512a432f900a4ad34339e
muhenge <mugunga5@gmail.com>
2020-06-29 11:38:33 +0200

    Update the friendship controller, to avoid user/friend duplication

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,7 +3,11 @@ class FriendshipsController < ApplicationController
 
   def create
     @friendship = current_user.friendships.build(friend_id: params[:friend_id])
-    if @friendship.save
+    @ft = Friendship.new
+    @ft.user_id = params[:friend_id]
+    @ft.friend_id = current_user.id
+
+    if @friendship.save && @ft.save
       flash.notice = 'Added friend.'
     else
       flash.alert = 'Unable to add friend.'
@@ -12,16 +16,24 @@ class FriendshipsController < ApplicationController
   end
 
   def update
-    @friendship = current_user.inverse_friendships.find(params[:id])
-    flash.notice = @friendship.update(status: true) ? 'Confirmed.' : 'Unable to Confirme.'
+    @ft = Friendship.where(user_id: current_user.id, friend_id: params[:id])
 
+    @friendship = current_user.inverse_friendships.find(params[:id])
+    flash[:notice] = if @friendship.update(status: true) && @ft.update(status: true)
+                       'Confirmed'
+                     else
+                       'Unable to comfirm'
+                     end
     redirect_to users_path
   end
 
   def destroy
+    @ft = Friendship.where(user_id: current_user.id, friend_id: params[:id])
+
     @friendship = current_user.inverse_friendships.find(params[:id])
-    @friendship.destroy
-    flash[:notice] = 'Rejected Invitation.'
+
+    flash[:notice] = @friendship.destroy && @ft.destroy ? flash[:notice] = 'Rejected' : 'Unable to reject'
+
     redirect_to users_path
   end
 end
